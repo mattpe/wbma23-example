392d2fcbbac7c0fee402659ccc917608bff07217
mattpe <mattpe@metropolia.fi>
2023-01-11 20:11:03 +0200

    add android safearea fix, flexbox styles

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {Platform, StyleSheet, SafeAreaView} from 'react-native';
 import List from './components/List';
 
 const App = () => {
-  //console.log('App starting!')
   return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <List />
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <List />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
   );
 };
 
@@ -20,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });
