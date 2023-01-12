d4450b4d9b8a9a7dbcc21031bdc3ac2f74ed9b52
mattpe <mattpe@metropolia.fi>
2023-01-12 11:34:28 +0200

    add basic tab icons

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Single from '../views/Single';
 
 const Tab = createBottomTabNavigator();
@@ -11,7 +12,22 @@ const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => {
+        return {
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = 'person';
+            }
+
+            return <Ionicons name={iconName} />;
+          },
+        };
+      }}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
