aa0a98f1b1e6615423f1e586283994f685556c6a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-02-08 09:55:25 +0200

    B started

--- views/MyFiles.js
+++ views/MyFiles.js
@@ -1,11 +1,13 @@
 import {View, Text} from 'react-native';
+import List from '../components/List';
+import PropTypes from 'prop-types';
 
-const MyFiles = () => {
-  return (
-    <View>
-      <Text>MyFiles</Text>
-    </View>
-  );
+const MyFiles = ({navigation}) => {
+  return <List navigation={navigation} myFilesOnly={true} />;
+};
+
+MyFiles.propTypes = {
+  navigation: PropTypes.object,
 };
 
 export default MyFiles;
